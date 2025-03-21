ffa20807b2054026985ace82ab53a6e806407045
diffth <diffth@nate.com>
2025-03-21 14:47:19 +0900

    Update Class_0321.cpp

--- Class1/Class_0321.cpp
+++ Class1/Class_0321.cpp
@@ -1,1 +1,48 @@
+#include <iostream>
+
+// C/C++ 언리얼 학습 진행 
+// 1.변수
+// 2.연산자
+// 3.제어문
+// 4.함수
+// 5.객체
+
+//void 리턴타입 없음, 빈값
+//void max1();	//한줄주석 ctrl + /
+
+/* 아스트릭, 별, 포인터, 블록주석, 곱셈연산자
+void max2();
+void max3();
+*/
+
+//한줄이동 alt + ↑, alt + ↓
+//한줄복제 ctrl + d
+
+//리턴타입 int 형
+int main()
+{
+	std::cout << "Hello World!\n";	//\n -> new line 줄바꿈
+	//<< 출력연산자
+	//>> 입력연산자
+	
+	//A = B;	// 대입연산자
+	//A == B;	// == 같다.
+	//A === B;	// === 타입 포함 같다.
+
+	int length = 5;	//변수 선언, 초기화
+	int height;		//변수 선언
+	height = 4;		//변수 초기화 -> x -> 오류
+
+	std::cout << length << std::endl;
+	std::cout << height << std::endl;	//초기화되지 않은 'height' 지역 변수를 사용했습니다.
+	//endl -> endline 줄바꿈
+
+	length = 10;
+	height = 8;
+
+	std::cout << length << std::endl;
+	std::cout << height << std::endl;
+
+	return 0;
+}
 
