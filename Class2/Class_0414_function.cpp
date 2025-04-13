7afe6f88b6a8bb74e24e5aaed0ebef0640822674
diffth <diffth@nate.com>
2025-04-13 21:08:46 +0900

    0413 printf commit

--- Class2/Class_0414_function.cpp
+++ Class2/Class_0414_function.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
 
 using namespace std;
 
@@ -17,7 +19,26 @@ int main()
 	int a = 3;	//변수 a 선언 및 초기화
 	int b = 4;	//변수 b 선언 및 초기화
 	c = Sum(a, b);	//함수의 호출
-	cout << c;		//c를 출력
-	return 0;
+	cout << c << endl;		//c를 출력
+	//return 0;
+
+	printf("Hello");
+	cout << endl;
+	printf("Hello\n");
+	printf("Hello\?");
+	cout << endl;
+	printf("\"Hello\"");
+	cout << endl;
+
+	char Initial = 'S';
+	printf("My Name is %c \n", Initial);
+	printf("I am %d years old\n", 15);
+	printf("Goodbye\n");
+	printf("%d %d %d\n", 10, 5, 20);
+
+	char initial;
+	scanf_s("%c", &initial);
+	printf("입력: %c", initial);
+
 
 }
