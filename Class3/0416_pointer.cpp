93dfae35315dc57611d6429422d65c20beb17ba7
diffth <diffth@nate.com>
2025-04-20 21:00:23 +0900

    0420 modify

--- Class3/0416_pointer.cpp
+++ Class3/0416_pointer.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int main()
-{
-	cout << "Hello" << endl;
-}
+//int main()
+//{
+//	cout << "Hello" << endl;
+//}
--- Class3/0420_.cpp
+++ Class3/0420_.cpp
@@ -2,7 +2,20 @@
 
 using namespace std;
 
+int* getArray(int length);
+
 int main()
 {
-	cout << "Hello" << endl;
+	//cout << "Hello" << endl;
+	int* arr_ptr = nullptr;
+	int length;
+	cin >> length; //배열의 길이
+	arr_ptr = getArray(length);
+
+	cout << arr_ptr << endl;
+}
+
+int* getArray(int length)
+{
+	return new int[length];
 }
